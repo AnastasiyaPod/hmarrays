cf8b9ac229679be7cb8bd2f55204ad4b2633eb97
AnastasiyaPod <an_pod12@mail.ru>
2023-09-29 00:03:34 +1000

    Rework

--- src/main/java/ru/netology/stats/hmarrays/services/StatService.java
+++ src/main/java/ru/netology/stats/hmarrays/services/StatService.java
@@ -1,51 +1,56 @@
 package ru.netology.stats.hmarrays.services;
 
-import static java.lang.Double.sum;
-
 public class StatService {
-    public double amountSales (double[] sales) {
+    public double amountSales(double[] sales) {
         double selling = 0;
         for (double element : sales) {
             selling += element;
         }
         return (long) selling;
     }
-       public double averageSale(double[] sales) {
+
+    public double averageSale(double[] sales) {
         return amountSales(sales) / sales.length;
     }
+
     public double maxSales(double[] sales) {
         int maxMonth = 0;
         for (int i = 0; i < sales.length; i++) {
             if (sales[i] >= sales[maxMonth]) {
                 maxMonth = i;
             }
-            Math.max(maxMonth, sales[i]);
         }
         return maxMonth + 1;
     }
+
     public int minSales(double[] sales) {
         int minMonth = 0;
         for (int i = 0; i < sales.length; i++) {
             if (sales[i] <= sales[minMonth]) {
                 minMonth = i;
             }
-            Math.max(minMonth, sales[i]);
         }
         return minMonth + 1;
     }
+
     public double belowAverageSales(double[] sales) {
+        double average = averageSale(sales);
         long month = 0;
         for (int i = 1; i < sales.length; i++) {
-            if (averageSale(sales) > sales[i])
+            if (average > sales[i]) {
                 month++;
+            }
         }
         return month;
     }
+
     public double aboveAverageSales(double[] sales) {
+        double average = averageSale(sales);
         long month = 0;
         for (int i = 1; i < sales.length; i++) {
-            if (averageSale(sales) < sales[i])
+            if (average < sales[i]) {
                 month++;
+            }
         }
         return month;
     }
--- src/test/java/ru/netology/stats/hmarrays/services/StatServiceTest.java
+++ src/test/java/ru/netology/stats/hmarrays/services/StatServiceTest.java
@@ -1,6 +1,8 @@
 package ru.netology.stats.hmarrays.services;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 public class StatServiceTest {
     @Test
     public void amountSales() {
@@ -10,14 +12,16 @@ public class StatServiceTest {
         long actualSum = (long) service.amountSales(sales);
         Assertions.assertEquals(expectedSum, actualSum);
     }
+
     @Test
     public void averageSale() {
         StatService service = new StatService();
-        double [] sales = {15, 11, 19.4, 21, 16, 18, 21.4, 18, 15, 31, 14, 5};
+        double[] sales = {15, 11, 19.4, 21, 16, 18, 21.4, 18, 15, 31, 14, 5};
         long expectedAverageSale = 17;
         long actualAverageSale = (long) service.averageSale(sales);
         Assertions.assertEquals(expectedAverageSale, actualAverageSale);
     }
+
     @Test
     public void maxSales() {
         StatService service = new StatService();
@@ -26,14 +30,16 @@ public class StatServiceTest {
         long actualMaxSale = (long) service.maxSales(sales);
         Assertions.assertEquals(expectedMaxSale, actualMaxSale);
     }
+
     @Test
     public void minSales() {
         StatService service = new StatService();
         double[] sales = {15, 11, 19.4, 31, 16, 18, 21.4, 18, 15, 31, 14, 5};
         long expectedMinSale = 12;
-        long actualMinSale = (long) service.minSales(sales);
+        long actualMinSale = service.minSales(sales);
         Assertions.assertEquals(expectedMinSale, actualMinSale);
     }
+
     @Test
     public void belowAverageSales() {
         StatService service = new StatService();
@@ -43,6 +49,7 @@ public class StatServiceTest {
         Assertions.assertEquals(expectedMonths, actualMonths);
 
     }
+
     @Test
     public void aboveAverageSales() {
         StatService service = new StatService();
